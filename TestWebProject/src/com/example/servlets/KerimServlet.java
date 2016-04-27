0147d9fe5ac2eadb541252227ada078057897a91
Kerim Gokarslan <kerim.gokarslan@boun.edu.tr>
2016-04-27 22:58:35 +0300

    KerimServlet is added

---
+++ TestWebProject/src/com/example/servlets/KerimServlet.java
@@ -0,0 +1,32 @@
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class KerimServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.getWriter().append("Hello world, my name is Kerim!");
+		super.doPost(req, resp);
+		
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3818282702335774732L;
+	public KerimServlet(){
+		super();
+	}
+
+}
