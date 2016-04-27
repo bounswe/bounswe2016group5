b6c78d208d9ab0459e3dff97e795214a776c6e41
Kerim Gokarslan <kerim.gokarslan@boun.edu.tr>
2016-04-27 23:08:29 +0300

    kerimservlet

--- TestWebProject/src/com/example/servlets/KerimServlet.java
+++ TestWebProject/src/com/example/servlets/KerimServlet.java
@@ -1,24 +1,29 @@
+package com.example.servlets;
+
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+@WebServlet("/kerim-gokarslan")
 public class KerimServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		//super.doGet(req, resp);
+		doPost(req, resp);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		resp.getWriter().append("Hello world, my name is Kerim!");
-		super.doPost(req, resp);
+		//super.doPost(req, resp);
 		
 	}
 	/**
