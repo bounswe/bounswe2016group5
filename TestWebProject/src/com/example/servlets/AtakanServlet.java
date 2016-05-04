3a963e6c5195884a7644c160c89bb08fe3b3bd35
atakanguney <atakanguney58@hotmail.com>
2016-05-04 15:39:27 +0300

    first try to do project

--- TestWebProject/src/com/example/servlets/AtakanServlet.java
+++ TestWebProject/src/com/example/servlets/AtakanServlet.java
@@ -1,15 +1,25 @@
 package com.example.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+
 /**
  * Servlet implementation class HelloServlet
  */
+@SuppressWarnings("deprecation")
 @WebServlet("/atakan-guney")
 public class AtakanServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,16 +37,27 @@ public class AtakanServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Welcome to Atakan Guney's page !!!!");
-		
+		PrintWriter out = response.getWriter();
+		String query = "SELECT ?item ?itemLabel ?pic WHERE { ?item ?x1 wd:Q146 . OPTIONAL { ?item wdt:P18 ?pic } SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } }";
+        String website = "https://query.wikidata.org/sparql?query=";
+		try {
+			String queryResult = getHTML("https://query.wikidata.org/sparql?query=SELECT%20?h%20WHERE{?h%20wdt:P31%20wd:Q3624078}");
+			out.println(queryResult);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public static String getHTML(String urlToRead) throws Exception {
+		 	 URL oracle = new URL(urlToRead);
+	        BufferedReader in = new BufferedReader(
+	        new InputStreamReader(oracle.openStream()));
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+	        String input;
+	        while ((input = in.readLine()) != null)
+	            System.out.println(input);
+	        in.close();
+	        return input;
+	   }
 }
