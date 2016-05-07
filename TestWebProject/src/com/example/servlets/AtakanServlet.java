3b0d55e24c2c9dcfbf588efe8cd2587dea9a6ebe
Kerim Gokarslan <kerim.gokarslan@boun.edu.tr>
2016-05-07 17:11:19 +0300

    added mysql connector and sparql link

--- TestWebProject/src/com/example/servlets/AtakanServlet.java
+++ TestWebProject/src/com/example/servlets/AtakanServlet.java
@@ -12,9 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Resource;
+//import org.apache.jena.rdf.model.Model;
+//import org.apache.jena.rdf.model.ModelFactory;
+//import org.apache.jena.rdf.model.Resource;
 
 /**
  * Servlet implementation class HelloServlet
@@ -23,23 +23,25 @@ import org.apache.jena.rdf.model.Resource;
 @WebServlet("/atakan-guney")
 public class AtakanServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AtakanServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public AtakanServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		String query = "SELECT ?item ?itemLabel ?pic WHERE { ?item ?x1 wd:Q146 . OPTIONAL { ?item wdt:P18 ?pic } SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } }";
-        String website = "https://query.wikidata.org/sparql?query=";
+		String website = "https://query.wikidata.org/sparql?query=";
 		try {
 			String queryResult = getHTML("https://query.wikidata.org/sparql?query=SELECT%20?h%20WHERE{?h%20wdt:P31%20wd:Q3624078}");
 			out.println(queryResult);
@@ -48,16 +50,16 @@ public class AtakanServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
-	
-	public static String getHTML(String urlToRead) throws Exception {
-		 	 URL oracle = new URL(urlToRead);
-	        BufferedReader in = new BufferedReader(
-	        new InputStreamReader(oracle.openStream()));
 
-	        String input;
-	        while ((input = in.readLine()) != null)
-	            System.out.println(input);
-	        in.close();
-	        return input;
-	   }
+	public static String getHTML(String urlToRead) throws Exception {
+		URL oracle = new URL(urlToRead);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				oracle.openStream()));
+
+		String input;
+		while ((input = in.readLine()) != null)
+			System.out.println(input);
+		in.close();
+		return input;
+	}
 }
--- TestWebProject/src/com/example/servlets/KerimServlet.java
+++ TestWebProject/src/com/example/servlets/KerimServlet.java
@@ -1,36 +1,84 @@
 package com.example.servlets;
 
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 @WebServlet("/kerim-gokarslan")
-public class KerimServlet extends HttpServlet{
+public class KerimServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//super.doGet(req, resp);
+		// super.doGet(req, resp);
 		doPost(req, resp);
 	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		if (req.getParameter("query") == null) {
+			save();
+			resp.sendRedirect("/TestWebProject/kerim-gokarslan.jsp");
+		} else {
+			String userQuery = req.getParameter("query");
+			resp.getWriter().append(userQuery);
+			String query = "https://query.wikidata.org/sparql?query=";
+			query += "SELECT%20?h%20WHERE{?h%20wdt:P31%20wd:Q3624078}";
+			URL url = new URL(query);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			String xmlResult = "";
+			String line;
+			while ((line = in.readLine()) != null)
+				xmlResult += line;
+			in.close();
+			resp.getWriter().append(xmlResult);
+		}
 		resp.getWriter().append("Hello world, my name is Kerim!");
-		//super.doPost(req, resp);
-		
+		// super.doPost(req, resp);
+
+	}
+	public void save(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+			
+		}
+		String url = "jdbc:mysql://group5db.cpp0ryqf88fx.us-west-2.rds.amazonaws.com:3306/";
+		//String url = "jdbc:mysql://asasdasdd.com:3306/";
+		String userName = "group5";
+		String password = "Cmpe352*";
+		String dbName = "group5";
+		String driver = "com.mysql.jdbc.Driver";
+		try {
+			Connection connection = DriverManager.getConnection(url + dbName, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("Succeed");
 	}
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3818282702335774732L;
-	public KerimServlet(){
+
+	public KerimServlet() {
 		super();
 	}
 
