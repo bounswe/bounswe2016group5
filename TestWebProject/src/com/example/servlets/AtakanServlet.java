77e6787b477d6018d2004946583d72ac209ec0c1
atakanguney <atakanguney58@hotmail.com>
2016-05-09 21:07:37 +0300

    First,get data from wikidata

--- TestWebProject/src/com/example/servlets/AtakanServlet.java
+++ TestWebProject/src/com/example/servlets/AtakanServlet.java
@@ -1,28 +1,30 @@
 package com.example.servlets;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.plaf.synth.SynthSeparatorUI;
 
-//import org.apache.jena.rdf.model.Model;
-//import org.apache.jena.rdf.model.ModelFactory;
-//import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 
 /**
- * Servlet implementation class HelloServlet
+ * Servlet implementation class AtakanServlet
  */
-@SuppressWarnings("deprecation")
 @WebServlet("/atakan-guney")
 public class AtakanServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static String jsonData = null;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -33,33 +35,52 @@ public class AtakanServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String inputTerm = request.getParameter("input");
+		String s1 = 
+					"PREFIX wd: <http://www.wikidata.org/entity/>\n"+
+					"PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n"+
+					"PREFIX wikibase: <http://wikiba.se/ontology#>\n"+
+					"PREFIX p: <http://www.wikidata.org/prop/>\n"+
+					"PREFIX ps: <http://www.wikidata.org/prop/statement/>\n"+
+					"PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n"+
+					"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+					"PREFIX bd: <http://www.bigdata.com/rdf#>\n"+
+
+					"#Most eponymous mathematicians\n"+
+					"SELECT ?eponym ?eponymLabel ?count ?sample ?sampleLabel\n"+
+					"WHERE\n"+
+					"{\n"+
+						"{\n"+
+						"SELECT ?eponym (COUNT(?item) as ?count) (SAMPLE(?item) AS ?sample)\n"+
+						"WHERE\n"+
+						"{\n"+
+							"?item wdt:P138 ?eponym.\n"+
+							"?eponym wdt:P106 wd:Q170790.\n"+
+						"}\n"+
+						"GROUP BY ?eponym\n"+
+						"}\n"+
+						"SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" }\n"+
+					"}\n"+
+					"ORDER BY DESC(?count)\n";
+		Query query = QueryFactory.create(s1); 
+		QueryExecution qExe = QueryExecutionFactory.sparqlService( "https://query.wikidata.org/sparql", query );
+		ResultSet results = qExe.execSelect();
+		
 		PrintWriter out = response.getWriter();
-		String query = "SELECT ?item ?itemLabel ?pic WHERE { ?item ?x1 wd:Q146 . OPTIONAL { ?item wdt:P18 ?pic } SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } }";
-		String website = "https://query.wikidata.org/sparql?query=";
-		try {
-			String queryResult = getHTML("https://query.wikidata.org/sparql?query=SELECT%20?h%20WHERE{?h%20wdt:P31%20wd:Q3624078}");
-			out.println(queryResult);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		out.println(results.getResultVars().toString());
+		while(results.hasNext()){
+			out.println(results.nextSolution());
 		}
 	}
 
-	public static String getHTML(String urlToRead) throws Exception {
-		URL oracle = new URL(urlToRead);
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				oracle.openStream()));
-
-		String input;
-		while ((input = in.readLine()) != null)
-			System.out.println(input);
-		in.close();
-		return input;
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
 	}
 }
