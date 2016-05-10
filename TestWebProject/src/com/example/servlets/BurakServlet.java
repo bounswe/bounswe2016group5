72c9dd97ae55c2b0013532bc05d0fbfabca1f3bc
Burak Suyunu <buraksuyunu@gmail.com>
2016-05-11 02:51:40 +0300

    updated, working properly

--- TestWebProject/src/com/example/servlets/BurakServlet.java
+++ TestWebProject/src/com/example/servlets/BurakServlet.java
@@ -28,7 +28,7 @@ import org.apache.jena.query.ResultSetFormatter;
  * 
  * @author buraksuyunu 
  *         2012400156 This code is written for the assignment 6 of the course
- *         CmpE 352 Spring '16. version 1.0
+ *         CmpE 352 Spring '16. version 1.5
  *	
  */
 @WebServlet("/burak-suyunu")
@@ -41,6 +41,7 @@ public class BurakServlet extends HttpServlet {
 	// Database credentials
 	static final String USER = "group5";
 	static final String PASS = "Cmpe352*";
+	private static final Object NULL = null;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -57,8 +58,10 @@ public class BurakServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		System.out.println("asdasd\n");
 		response.setContentType("text/html");
+		/*
+		 * Enters when the user enters a query
+		 */
 		if (request.getParameter("query") != null) {
 			String s1 = "PREFIX wd: <http://www.wikidata.org/entity/>\n"
 					+ "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n"
@@ -70,39 +73,81 @@ public class BurakServlet extends HttpServlet {
 
 					"SELECT ?human ?humanLabel ?awardEditionLabel ?awardLabel ?awardWork ?awardWorkLabel ?time\n"
 					+ "WHERE\n{\n{\n"
-					+ "SELECT (SAMPLE(?human) AS ?human) ?award ?awardWork (SAMPLE(?awardEdition) AS ?awardEdition) (SAMPLE(?time) AS ?time)"
+					+ "SELECT (SAMPLE(?H) AS ?human) ?award ?awardWork (SAMPLE(?aE) AS ?awardEdition) (SAMPLE(?T) AS ?time)"
 					+ "WHERE {\n"
 					+ "?award wdt:P31 wd:Q19020 . \n{\n"
-					+ "?human p:P166 ?awardStat . \n"
+					+ "?H p:P166 ?awardStat . \n"
 					+ "?awardStat ps:P166 ?award . \n"
-					+ "?awardStat pq:P805 ?awardEdition . \n"
+					+ "?awardStat pq:P805 ?aE . \n"
 					+ "?awardStat pq:P1686 ?awardWork . \n"
-					+ "?human wdt:P31 wd:Q5 . \n"
+					+ "?H wdt:P31 wd:Q5 . \n"
 					+ "} UNION { \n"
 					+ "?awardWork wdt:P31 wd:Q11424 . \n"
 					+ "?awardWork p:P166 ?awardStat . \n"
 					+ "?awardStat ps:P166 ?award . \n"
-					+ "?awardStat pq:P805 ?awardEdition . \n"
+					+ "?awardStat pq:P805 ?aE . \n"
 					+ "}\n OPTIONAL {\n"
-					+ "?awardEdition wdt:P585 ?time . \n}\n}\n"
+					+ "?aE wdt:P585 ?T . \n}\n}\n"
 					+ "GROUP BY ?awardWork ?award \n}\n"
 					+ "SERVICE wikibase:label {\n"
 					+ "bd:serviceParam wikibase:language \"en\" .\n}\n}\n"
 					+ "ORDER BY DESC(?time)\n LIMIT 200\n";
-			System.out.println("11111\n");
 			
 			Query query = QueryFactory.create(s1);
-			System.out.println("22222\n");
 			QueryExecution qExe = QueryExecutionFactory.sparqlService("https://query.wikidata.org/sparql", query);
-			System.out.println("33333\n");
 			ResultSet results = qExe.execSelect();
 			
 			String query1 = request.getParameter("query");
-			createResultTable(results, out, query1);
+			//createResultTable(results, out, query1);
+			
+			String table = "<form name=\"ftable\" method=\"post\" action=\"/TestWebProject/burak-suyunu\">";
+			table += "<table border=\"1\" style=\"width:100%\">\n" + "<tr>\n" + "<th>Select</th>\n" + "<th>human</th>\n"
+					+ "<th>awardEditionLabel</th>\n" + "<th>awardLabel</th>\n"
+					+ "<th>awardWork</th>\n" + "<th>time</th>\n" + "</tr>\n";
 
-		} else if (request.getParameter("selected") != null) {
+			while (results.hasNext()) {
+				QuerySolution currentSolution = results.nextSolution();
+				String human = "@";
+				String humanLabel  = "@";
+				if(currentSolution.getResource("?human") != null)
+					human = currentSolution.getResource("?human").toString();
+				if(currentSolution.getLiteral("?humanLabel") != null)
+					humanLabel = currentSolution.getLiteral("?humanLabel").toString();
+				String awardEditionLabel = currentSolution.getLiteral("?awardEditionLabel").toString();
+				String awardLabel = currentSolution.getLiteral("?awardLabel").toString();
+				String awardWork = currentSolution.getResource("?awardWork").toString();
+				String awardWorkLabel = currentSolution.getLiteral("?awardWorkLabel").toString();
+				String time = currentSolution.getLiteral("?time").toString();
+				
+				if (!humanLabel.toLowerCase().contains(query1.toLowerCase()))
+					if (!awardEditionLabel.toLowerCase().contains(query1.toLowerCase()))
+						if (!awardWorkLabel.toLowerCase().contains(query1.toLowerCase()))
+							continue;
+				table += "<tr>\n" + "<td>\n" + "<input type=\"checkbox\" name=\"selected\" value=\"" + human + "," + humanLabel
+						+ "," + awardEditionLabel + "," + awardLabel  + "," + awardWork
+						+ "," + awardWorkLabel + "," + time + "," + "\"/>" + "</td>\n";
+				
+				
+				table += "<td>\n" + "<a href=\"" + human + "\">" + humanLabel.substring(0, humanLabel.indexOf('@')) + "</td>\n";
+				table += "<td>\n" + awardEditionLabel.substring(0, awardEditionLabel.indexOf('@')) + "</td>\n";
+				table += "<td>\n" + awardLabel.substring(0, awardLabel.indexOf('@')) + "</td>\n";
+				table += "<td>\n" + "<a href=\"" + awardWork + "\">" + awardWorkLabel.substring(0, awardWorkLabel.indexOf('@')) + "</td>\n";
+				table += "<td>\n" + time.substring(0, time.indexOf('^')) + "</td>\n";
+				table += "</tr>\n";
+			}
+			table += "</table>";
+			table += "<table><tr><td><input id=\"submit\" name=\"submit\" type=\"submit\" value=\"save\"/></td></tr></table>";
+			table += "</form>";
+			out.println(table);
+			
+
+		}
+		/*
+		 * Enters when the user wants to save data to database
+		 */
+		else if (request.getParameter("selected") != null) {
 			String[] selectedItems = request.getParameterValues("selected");
-			out.println(Arrays.toString(selectedItems));
+			//out.println(Arrays.toString(selectedItems));
 
 			Connection conn = null;
 			Statement stmt = null;
@@ -116,15 +161,11 @@ public class BurakServlet extends HttpServlet {
 				String sql = "";
 				for (int i = 0; i < selectedItems.length; i++) {
 					String[] elts = selectedItems[i].split(",");
-					sql = "SELECT human, humanLabel, awardEditionLabel, awardLabel, awardWork, awardWorkLabel, time FROM burak_awards WHERE nameURI='"
-							+ elts[0] + "';";
-					java.sql.ResultSet rs = stmt.executeQuery(sql);
-					if (!rs.next()) {
+
 						sql = "INSERT INTO burak_awards " + "VALUES ( \"" + elts[0] + "\", \"" + elts[1] + "\", \"" + elts[2]
 								+ "\", \"" + elts[3] + "\", \"" + elts[4] + "\", \"" + elts[5] + "\", \"" + elts[6] + "\")";
 						stmt.executeUpdate(sql);
 
-					}
 				}
 				out.print("<br><br>DATA SAVED<br>Please <a href=\"/TestWebProject/burak-suyunu\"> click here</a> to redirect.");
 
@@ -145,7 +186,11 @@ public class BurakServlet extends HttpServlet {
 					se.printStackTrace();
 				}
 			}
-		} else if (request.getParameter("show") != null) {
+		}
+		/*
+		 * Enters when the user wants to see the saved data
+		 */
+		else if (request.getParameter("show") != null) {
 			Connection conn = null;
 			Statement stmt = null;
 			try {
@@ -156,8 +201,8 @@ public class BurakServlet extends HttpServlet {
 				java.sql.ResultSet rs = stmt.executeQuery(sql);
 				String table = "<form name=\"ftable\" method=\"post\" action=\"/TestWebProject/burak-suyunu\">";
 				table += "<table border=\"1\" style=\"width:100%\">\n" + "<tr>\n" + "<th>human</th>\n"
-						+ "<th>humanLabel</th>\n" + "<th>awardEditionLabel</th>\n" + "<th>awardLabel</th>\n"
-						+ "<th>awardWork</th>\n" + "<th>awardWorkLabel</th>\n" + "<th>time</th>\n" + "</tr>\n";
+						+ "<th>awardEditionLabel</th>\n" + "<th>awardLabel</th>\n"
+						+ "<th>awardWork</th>\n" + "<th>time</th>\n" + "</tr>\n";
 
 				while (rs.next()) {
 					// Retrieve by column name
@@ -170,11 +215,11 @@ public class BurakServlet extends HttpServlet {
 					String time = rs.getString("time");
 
 					table += "<tr>\n";
-					table += "<td>\n" + "<a href=\"" + human + "\">" + humanLabel + "</td>\n";
-					table += "<td>\n" + awardEditionLabel + "</td>\n";
-					table += "<td>\n" + awardLabel + "</td>\n";
-					table += "<td>\n" + "<a href=\"" + awardWork + "\">" + awardWorkLabel + "</td>\n";
-					table += "<td>\n" + time + "</td>\n";
+					table += "<td>\n" + "<a href=\"" + human + "\">" + humanLabel.substring(0, humanLabel.indexOf('@')) + "</td>\n";
+					table += "<td>\n" + awardEditionLabel.substring(0, awardEditionLabel.indexOf('@')) + "</td>\n";
+					table += "<td>\n" + awardLabel.substring(0, awardLabel.indexOf('@')) + "</td>\n";
+					table += "<td>\n" + "<a href=\"" + awardWork + "\">" + awardWorkLabel.substring(0, awardWorkLabel.indexOf('@')) + "</td>\n";
+					table += "<td>\n" + time.substring(0, time.indexOf('^')) + "</td>\n";
 					table += "</tr>\n";
 				}
 				table += "</table>";
@@ -199,11 +244,22 @@ public class BurakServlet extends HttpServlet {
 					se.printStackTrace();
 				}
 			}
-		} else {
+		}
+		/*
+		 * Enters when the user choose to delete saved data
+		 */
+		else if (request.getParameter("delete") != null){
+			emptyTable();
+			out.println("Data Truncated!!!<br><br><a href=\"/TestWebProject/burak-suyunu\"> click here</a> to redirect.");
+		}
+		else {
 			response.sendRedirect("/TestWebProject/burak-suyunu.jsp");
 		}
 	}
 
+	/*
+	 * Creates new DB table burak_awards to hold saved data
+	 */
 	public void createTable() {
 		Connection conn = null;
 		Statement stmt = null;
@@ -235,45 +291,40 @@ public class BurakServlet extends HttpServlet {
 			}
 		}
 	}
+	
+	/*
+	 * Truncates burak_awards table
+	 */
+	public void emptyTable() {
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
+			String sql = "TRUNCATE TABLE  burak_awards";
 
-	public void createResultTable(ResultSet results, PrintWriter out, String searchQuery) {
-		String table = "<form name=\"ftable\" method=\"post\" action=\"/TestWebProject/burak-suyunu\">";
-		table += "<table border=\"1\" style=\"width:100%\">\n" + "<tr>\n" + "<th>human</th>\n"
-				+ "<th>humanLabel</th>\n" + "<th>awardEditionLabel</th>\n" + "<th>awardLabel</th>\n"
-				+ "<th>awardWork</th>\n" + "<th>awardWorkLabel</th>\n" + "<th>time</th>\n" + "</tr>\n";
+			stmt.executeUpdate(sql);
 
-		while (results.hasNext()) {
-			QuerySolution currentSolution = results.nextSolution();
-			String human = currentSolution.getLiteral("?human").toString();
-			String humanLabel = currentSolution.getResource("?humanLabel").toString();
-			String awardEditionLabel = currentSolution.getLiteral("?awardEditionLabel").toString();
-			String awardLabel = currentSolution.getResource("?awardLabel").toString();
-			String awardWork = currentSolution.getLiteral("?awardWork").toString();
-			String awardWorkLabel = currentSolution.getLiteral("?awardWorkLabel").toString();
-			String time = currentSolution.getResource("?time").toString();
-			
-			if (!humanLabel.toLowerCase().contains(searchQuery.toLowerCase()))
-				if (!awardEditionLabel.toLowerCase().contains(searchQuery.toLowerCase()))
-					if (!awardWorkLabel.toLowerCase().contains(searchQuery.toLowerCase()))
-						continue;
-			table += "<tr>\n" + "<td>\n" + "<input type=\"checkbox\" name=\"selected\" value=\"" + human + "," + humanLabel
-					+ "," + awardEditionLabel + "," + awardLabel  + "," + awardWork
-					+ "," + awardWorkLabel + "," + time + "," + "\"/>" + "</td>\n" + "<td>\n";
-			
-			table += "<tr>\n";
-			table += "<td>\n" + "<a href=\"" + human + "\">" + humanLabel + "</td>\n";
-			table += "<td>\n" + awardEditionLabel + "</td>\n";
-			table += "<td>\n" + awardLabel + "</td>\n";
-			table += "<td>\n" + "<a href=\"" + awardWork + "\">" + awardWorkLabel + "</td>\n";
-			table += "<td>\n" + time + "</td>\n";
-			table += "</tr>\n";
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					conn.close();
+			} catch (SQLException se) {
+			}
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
 		}
-		table += "</table>";
-		table += "<table><tr><td><input id=\"submit\" name=\"submit\" type=\"submit\" value=\"save\"/></td></tr></table>";
-		table += "</form>";
-		out.println(table);
-
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
