154c210b0e3e2e2354f06465e3e83f672333e11f
sahin batmaz <cyberoyal@windowslive.com>
2016-11-02 08:52:28 +0200

    Android - Forget password activity is finished

--- DigestAndroid/app/src/main/java/digest/digestandroid/ForgetPasswordActivity.java
+++ DigestAndroid/app/src/main/java/digest/digestandroid/ForgetPasswordActivity.java
@@ -1,9 +1,13 @@
 package digest.digestandroid;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -20,7 +24,16 @@ public class ForgetPasswordActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.inject(this);
 
+        _resetpasswordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                resetpassword();
+            }
+        });
     }
 
     public void resetpassword(){
@@ -28,17 +41,52 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         Log.d(TAG, "Reset password");
 
         if (!validate()) {
-     
-	     return;
+            onResetPasswordFailed();
+            return;
         }
 
-     
+        _resetpasswordButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(ForgetPasswordActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Resetting password...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        onResetPasswordFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+
+    }
+
+    public void onResetPasswordSuccess(){
+        _resetpasswordButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onResetPasswordFailed(){
+        Toast.makeText(getBaseContext(), "Resetting password failed", Toast.LENGTH_LONG).show();
+        _resetpasswordButton.setEnabled(true);
     }
 
     public boolean validate(){
         boolean valid = true;
 
         String email = _emailText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
 
 
         return valid;
