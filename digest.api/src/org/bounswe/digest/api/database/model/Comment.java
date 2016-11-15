b243bd462ec5841cd818adeef275dc9b03fe841f
ozan <ozanbulut@outlook.com.tr>
2016-11-15 17:25:52 +0300

    Comment updated

--- digest.api/src/org/bounswe/digest/api/database/model/Comment.java
+++ digest.api/src/org/bounswe/digest/api/database/model/Comment.java
@@ -5,11 +5,14 @@ public class Comment {
 	private int uid;
 	private String username;
 	private String text;
+	private int rate;
 	
-	public Comment(int uid,String username,String text){
+	public Comment(int id,int uid,String username,String text,int rate){
+		this.id=id;
 		this.setUid(uid);
 		this.setText(text);
 		this.setUsername(username);
+		this.rate=rate;
 	}
 
 	public String getText() {
@@ -42,6 +45,14 @@ public class Comment {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
 	}
 	
 	
