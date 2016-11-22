7b6d26fbde149dd3c33118c63ef7c7887db8396b
Mavera07 <cyberoyal@windowslive.com>
2016-11-22 22:55:45 +0300

    Home page-home fragment- implementing topic list with cardview visiualization.

---
+++ DigestAndroid/app/src/main/java/digest/digestandroid/HomeAdapter.java
@@ -0,0 +1,83 @@
+package digest.digestandroid;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import digest.digestandroid.Models.Topic;
+
+/**
+ * Created by sahin on 22.11.2016.
+ */
+
+public class HomeAdapter extends RecyclerView
+        .Adapter<HomeAdapter.DataObjectHolder>{
+
+    private static String LOG_TAG = "HomeAdapter";
+    private ArrayList<Topic> homeDataset;
+    private static HomeClickListener homeClickListener;
+
+    public static class DataObjectHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView imageView;
+        TextView textView1;
+        TextView textView2;
+
+        public DataObjectHolder(View itemView){
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.home_card_image);
+            textView1 = (TextView) itemView.findViewById(R.id.home_text_view1);
+            textView2 = (TextView) itemView.findViewById(R.id.home_text_view2);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v){
+            homeClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public void setOnItemClickListener(HomeClickListener homeClickListener){
+        this.homeClickListener = homeClickListener;
+    }
+    public HomeAdapter(ArrayList<Topic> homeDataset){
+        this.homeDataset = homeDataset;
+    }
+
+    @Override
+    public DataObjectHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.home_card_view_row,parent,false);
+        DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataObjectHolder holder, int position){
+        holder.textView1.setText(homeDataset.get(position).getTitle());
+        holder.textView2.setText(homeDataset.get(position).getBody());
+    }
+
+    public void addItem(Topic dataObj, int index){
+        homeDataset.add(index,dataObj);
+        notifyItemInserted(index);
+    }
+    public void deleteItem(int index){
+        homeDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount(){
+        return homeDataset.size();
+    }
+
+    public interface HomeClickListener {
+        public void onItemClick(int position, View v);
+    }
+
+}
--- DigestAndroid/app/src/main/java/digest/digestandroid/fragments/RegisteredHomeHomeFragment.java
+++ DigestAndroid/app/src/main/java/digest/digestandroid/fragments/RegisteredHomeHomeFragment.java
@@ -3,32 +3,44 @@ package digest.digestandroid.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import digest.digestandroid.Models.Comment;
 import digest.digestandroid.Models.Topic;
+import digest.digestandroid.Models.User;
 import digest.digestandroid.R;
+import digest.digestandroid.HomeAdapter;
 
 
 public class RegisteredHomeHomeFragment extends Fragment {
 
     protected View rootView;
 
-    private RecyclerView homeRecyclerView;
+    private ArrayList<Topic> homeTopics = new ArrayList<Topic>();
     private RecyclerView.Adapter homeAdapter;
+
+    private RecyclerView homeRecyclerView;
     private RecyclerView.LayoutManager homeLayoutManager;
+
     private static String Home_Fragment = "HomeFragment";
 
 
-    public RegisteredHomeHomeFragment() {
-
-    }
+    public RegisteredHomeHomeFragment() {}
 
     public RegisteredHomeHomeFragment getInstance(){
 
@@ -37,8 +49,33 @@ public class RegisteredHomeHomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
         rootView = inflater.inflate(R.layout.fragment_home_home, container, false);
 
+        homeRecyclerView = (RecyclerView) rootView.findViewById(R.id.home_recycler_view);
+        homeLayoutManager = new LinearLayoutManager(getActivity());
+        homeRecyclerView.setLayoutManager(homeLayoutManager);
+        homeRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        //--------------
+        Topic topic;
+        User user = new User();
+        user.setUsername("Burki");
+        topic = new Topic();
+        topic.setOwner(user);
+        topic.setImage_url("http://i.dailymail.co.uk/i/pix/2016/04/12/23/3319F89C00000578-3536787-image-m-19_1460498410943.jpg");
+        topic.setTitle("TITLEEEEEE");
+        topic.setBody("HEBELE HUBELE CCOK GUZEL BI TEXT BU");
+        topic.setMaterials(new ArrayList<String>());
+        topic.getMaterials().add("111111111111111");
+        topic.getMaterials().add("22222222222222222");
+        topic.getMaterials().add("333333333333");
+        homeTopics.add(topic);
+        //--------------
+
+        homeAdapter = new HomeAdapter(homeTopics);
+        homeRecyclerView.setAdapter(homeAdapter);
 
         return rootView;
     }
@@ -46,10 +83,31 @@ public class RegisteredHomeHomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.fragment_home_home);
 
 
     }
+
+    // TODO initialize homeTopics , implement onResume, implement adapter
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        ((HomeAdapter) homeAdapter).setOnItemClickListener(new HomeAdapter.HomeClickListener(){
+           @Override
+            public void onItemClick(int pos, View v){
+
+           }
+        });
+    }
+
+
+
+
+
+
+
+
 
 
 }
