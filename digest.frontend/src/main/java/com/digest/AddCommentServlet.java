a01f79b5ecc3f6010ee6fa96fa49736c8b1616eb
Şeyma Ertem <seyma.ertem@gmail.com>
2016-11-23 02:26:10 +0200

    Add files via upload

---
+++ digest.frontend/src/main/java/com/digest/AddCommentServlet.java
@@ -0,0 +1,81 @@
+package com.digest;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@WebServlet("/AddCommentServlet")
+public class AddCommentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddCommentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		StringBuffer bf = new StringBuffer();
+		bf.append("http://digest.us-east-1.elasticbeanstalk.com/digest.api/?f=add_comment&");
+		Enumeration<String> names = request.getParameterNames();
+		
+		while (names.hasMoreElements()) {
+			String attr = names.nextElement();
+			String value = request.getParameter(attr);
+			bf.append(attr + "=" + value);
+			if (names.hasMoreElements())
+				bf.append("&");
+		}
+		String url = bf.toString();
+		System.out.println(url);
+		URL connpage = new URL(url);
+		HttpURLConnection urlcon = (HttpURLConnection) connpage.openConnection();
+		
+		int responseCode = urlcon.getResponseCode();
+		System.out.println(responseCode);
+		if (responseCode == 200 ) {
+			response.sendRedirect("/ViewTopicServlet");
+		} else if (responseCode == 400) {
+			HttpSession session = request.getSession(true);
+			String errMsg = "Unexpected Error occured!!";
+			session.setAttribute("error", errMsg);
+			//response.sendRedirect("view-topic.jsp");
+		}
+		
+		
+		
+	}
+}
