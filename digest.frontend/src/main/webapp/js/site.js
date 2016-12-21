077d1d4a537b001443a8d8059f6ed90ded0deb6b
Şeyma Ertem <seyma.ertem@gmail.com>
2016-12-21 02:59:53 +0200

    Subscribed Channels

--- digest.frontend/src/main/webapp/js/site.js
+++ digest.frontend/src/main/webapp/js/site.js
@@ -11,23 +11,31 @@
 		$('#search').keyup(function () {
 			var searchField = $('#search').val();
 			var showData = $('#show-data');
+			
+			if(searchField === ""){
+				showData.empty();
+			}
+			
 
 			$.ajax({url:'SearchServlet?f=search_with_tag&tag='+searchField,
 					dataType: 'json',
 					success: function(data){
+
+					
 						
 					showData.empty();
 					var content = '';
-					$.each(data,function(key,val){					
-	       				content  += '<a class="list-group-item" href="ViewTopicServlet?topic_id='+val.id+'">' + val.header + '</a>';
+					$.each(data,function(key,val){	
+						//window.alert(val.);
+	       				content  += '<a class="list-group-item" href="ViewTopicServlet?topic_id='+val.id+'">' + val.name + '</a>';
 				
 					});
-	        		
 					var list = $('<div class="list-group" />').html(content);
 	        		showData.append(list);
 
 				}
 			});
+			
 
 		});
 		
@@ -36,6 +44,23 @@
 				"href": "search.jsp?search_field=" + $('#search').val()
 			});			
 		});
+		
+
+			$.getJSON('ChannelServlet?f=get_subscribed_channels',function(data){
+				var sub_channels = $('#sub_channels');
+				var content = '';
+				$.each(data,function(key,val){
+					if(val != null){
+						content = '<li class="list-group-item"><a href="channel.jsp?cid='+val.id+'">'+val.name+'</a></li>';
+						sub_channels.append($('<ul class="list-group" />').html(content));
+					};
+						
+				});
+			});
+			
+		
+		
+		
 
 		
 	});
