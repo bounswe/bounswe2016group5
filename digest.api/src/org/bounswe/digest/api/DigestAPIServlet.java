9be9613bc8d2e8439d93dfba9899a20ffc848092
Kerim Gokarslan <kerimgokarslan@gmail.com>
2016-12-21 21:39:11 +0200

    Documentation update

--- digest.api/src/org/bounswe/digest/api/DigestAPIServlet.java
+++ digest.api/src/org/bounswe/digest/api/DigestAPIServlet.java
@@ -38,75 +38,73 @@ import org.json.JSONObject;
 import com.google.gson.Gson;
 
 @WebServlet("/")
+/**
+ * Handles HTTP requests.
+ * 
+ * @author Kerim Gokarslan <kerim.gokarslan@boun.edu.tr>
+ * @author Ozan Bulut <ozan.bulut@boun.edu.tr>
+ *
+ */
 public class DigestAPIServlet extends HttpServlet {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 8204342910649235663L;
-
+	/**
+	 * Handles get requests
+	 * @param req Request
+	 * @param resp Response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String f = req.getParameter(DigestParameters.FUNC);
 		if (f == null || f.length() == 0) {
 			resp.getWriter().append("Welcome to Digest API");
 		} else if (f.equals(DigestParameters.GET_TOPICS_OF_USER)) {
+			int ruid = Integer.parseInt(req.getParameter(DigestParameters.RUID));
+			resp.getWriter().append(TopicJDBC.getTopicsWithUser(ruid));
+		} else if (f.equals(DigestParameters.GET_COMMENTS_OF_TOPIC)) {
+			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
+			resp.getWriter().append(CommentJDBC.getCommentsOfTopic(tid));
+		} else if (f.equals(DigestParameters.GET_TOPIC)) {
+			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
+			resp.getWriter().append(TopicJDBC.getTopic(tid));
+		} else if (f.equals(DigestParameters.GET_USERNAME)) {
+			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
+			resp.getWriter().append(UserJDBC.getUserName(uid));
+		} else if (f.equals(DigestParameters.ADD_MEDIA)) {
+			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
+			String url = req.getParameter(DigestParameters.URL);
+			if (TopicJDBC.addMedia(tid, url) == 0) {
+				resp.setStatus(200);
+			} else {
+				resp.setStatus(400);
+			}
+		} else if (f.equals(DigestParameters.GET_QUIZ)) {
 			// int uid =
 			// Integer.parseInt(req.getParameter(DigestParameters.UID));
 			// String session = req.getParameter(DigestParameters.SESSION);
-			int ruid = Integer.parseInt(req.getParameter(DigestParameters.RUID));
+			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
 			// if (UserJDBC.isSessionValid(uid, session)) {
-			resp.getWriter().append(TopicJDBC.getTopicsWithUser(ruid));
+			resp.getWriter().append(QuizJDBC.getQuizzesOfTopic(tid));
 			// } else {
 			// resp.getWriter().append(invalidSession());
 
 			// }
-		} else if (f.equals(DigestParameters.GET_COMMENTS_OF_TOPIC)) {
-			//int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
-			//String session = req.getParameter(DigestParameters.SESSION);
-			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
-			//if (UserJDBC.isSessionValid(uid, session)) {
-				resp.getWriter().append(CommentJDBC.getCommentsOfTopic(tid));
-				// resp.getWriter().append(TopicJDBC.getCommentsOfTopic(tid));
-			//} else {
-				//resp.getWriter().append(invalidSession());
-
-			//}
-		}else if (f.equals(DigestParameters.GET_TOPIC)) {
-			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
-			resp.getWriter().append(TopicJDBC.getTopic(tid));
-		}else if (f.equals(DigestParameters.GET_USERNAME)) {
-			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
-			resp.getWriter().append(UserJDBC.getUserName(uid));
-		}else if (f.equals(DigestParameters.ADD_MEDIA)) {
-			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
-			String url = req.getParameter(DigestParameters.URL);
-			if(TopicJDBC.addMedia(tid, url)==0){
-				resp.setStatus(200);
-			}else{
-				resp.setStatus(400);
-			}
-		}else if (f.equals(DigestParameters.GET_QUIZ)) {
-			//int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
-			//String session = req.getParameter(DigestParameters.SESSION);
-			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
-			//if (UserJDBC.isSessionValid(uid, session)) {
-				resp.getWriter().append(QuizJDBC.getQuizzesOfTopic(tid));
-		//	} else {
-			//	resp.getWriter().append(invalidSession());
-
-			//}		
 		} else if (f.equals(DigestParameters.GET_RECENT_TOPICS)) {
 			// int uid =
 			// Integer.parseInt(req.getParameter(DigestParameters.UID));
 			// String session = req.getParameter(DigestParameters.SESSION);
 			int count = Integer.parseInt(req.getParameter(DigestParameters.COUNT));
-			//if (UserJDBC.isSessionValid(uid, session)) {
-				resp.getWriter().append(TopicJDBC.getRecentTopics(count));
-			//} else {
-				//resp.getWriter().append(invalidSession());
-			//}
-		} else if (f.equals(DigestParameters.ADD_COMMENT)){
+			// if (UserJDBC.isSessionValid(uid, session)) {
+			resp.getWriter().append(TopicJDBC.getRecentTopics(count));
+			// } else {
+			// resp.getWriter().append(invalidSession());
+			// }
+		} else if (f.equals(DigestParameters.ADD_COMMENT)) {
 			String body = req.getParameter(DigestParameters.BODY);
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			int ucid = Integer.parseInt(req.getParameter(DigestParameters.UCID));
@@ -116,8 +114,8 @@ public class DigestAPIServlet extends HttpServlet {
 				resp.setStatus(200);
 			} else {
 				resp.setStatus(400);
-			}	
-		}else if (f.equals(DigestParameters.ADD_SUBSCRIBER)) {
+			}
+		} else if (f.equals(DigestParameters.ADD_SUBSCRIBER)) {
 			// int uid =
 			// Integer.parseInt(req.getParameter(DigestParameters.UID));
 			// String session = req.getParameter(DigestParameters.SESSION);
@@ -125,17 +123,17 @@ public class DigestAPIServlet extends HttpServlet {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 
 			// if (UserJDBC.isSessionValid(uid, session)) {
-			if(TopicJDBC.addSubscriberToTopic(tid, uid) != -1){
+			if (TopicJDBC.addSubscriberToTopic(tid, uid) != -1) {
 				resp.setStatus(200);
-				
-			}else{
+
+			} else {
 				resp.setStatus(400);
 			}
 			// } else {
-				// resp.getWriter().append(invalidSession());
+			// resp.getWriter().append(invalidSession());
 
-			//}	
-		}else if (f.equals(DigestParameters.GET_SUBSCRIBED_TOPICS)) {
+			// }
+		} else if (f.equals(DigestParameters.GET_SUBSCRIBED_TOPICS)) {
 			// int uid =
 			// Integer.parseInt(req.getParameter(DigestParameters.UID));
 			// String session = req.getParameter(DigestParameters.SESSION);
@@ -144,9 +142,9 @@ public class DigestAPIServlet extends HttpServlet {
 			// if (UserJDBC.isSessionValid(uid, session)) {
 			resp.getWriter().append(TopicJDBC.getSubscribedTopics(uid));
 			// } else {
-				// resp.getWriter().append(invalidSession());
+			// resp.getWriter().append(invalidSession());
 
-			//}	
+			// }
 		} else if (f.equals(DigestParameters.GET_TOPICS_WITH_TAG)) {
 			// int uid =
 			// Integer.parseInt(req.getParameter(DigestParameters.UID));
@@ -158,10 +156,10 @@ public class DigestAPIServlet extends HttpServlet {
 			Gson gson = new Gson();
 			resp.getWriter().append(gson.toJson(topics));
 			// } else {
-				// resp.getWriter().append(invalidSession());
+			// resp.getWriter().append(invalidSession());
 
-			//}	
-		}else if (f.equals(DigestParameters.LOGIN)) {
+			// }
+		} else if (f.equals(DigestParameters.LOGIN)) {
 			String username = req.getParameter(DigestParameters.USERNAME);
 			String password = req.getParameter(DigestParameters.PASSWORD);
 			resp.getWriter().append(UserJDBC.login(username, password));
@@ -179,101 +177,106 @@ public class DigestAPIServlet extends HttpServlet {
 			} else {
 				resp.setStatus(400);
 			}
-		} else if (f.equals(DigestParameters.GET_TAG_SUGGESTIONS)){ 
+		} else if (f.equals(DigestParameters.GET_TAG_SUGGESTIONS)) {
 			String queryText = req.getParameter(DigestParameters.BODY);
 			queryText.replaceAll("_", " ");
 			CalaisAPI httpClientPost = new CalaisAPI();
 			Gson gson = new Gson();
 			resp.getWriter().append(gson.toJson(httpClientPost.extractTags(queryText)));
-		} else if (f.equals(DigestParameters.GET_TAG_ENTITIES)){
+		} else if (f.equals(DigestParameters.GET_TAG_ENTITIES)) {
 			String queryText = req.getParameter(DigestParameters.TAG);
 			ConceptNetAPI httpClientPost = new ConceptNetAPI();
 			Gson gson = new Gson();
 			resp.getWriter().append(gson.toJson(httpClientPost.extractEntities(queryText)));
-		}else if (f.equals(DigestParameters.GET_RELATED_ENTITIES)){
+		} else if (f.equals(DigestParameters.GET_RELATED_ENTITIES)) {
 			String queryText = req.getParameter(DigestParameters.TAG);
 			ConceptNetAPI httpClientPost = new ConceptNetAPI();
 			Gson gson = new Gson();
 			resp.getWriter().append(gson.toJson(httpClientPost.getRelatedEntities(queryText)));
-		}else if(f.equals(DigestParameters.ADD_CHANNEL)){
-			int uid=Integer.parseInt(req.getParameter(DigestParameters.UID));
-			String name=req.getParameter(DigestParameters.NAME);
+		} else if (f.equals(DigestParameters.ADD_CHANNEL)) {
+			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
+			String name = req.getParameter(DigestParameters.NAME);
 			ChannelJDBC.addChannel(uid, name);
-		}else if(f.equals(DigestParameters.RATE_COMMENT)){
+		} else if (f.equals(DigestParameters.RATE_COMMENT)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
 			CommentJDBC.rateComment(uid, cid);
-		}else if(f.equals(DigestParameters.UNRATE_COMMENT)){
+		} else if (f.equals(DigestParameters.UNRATE_COMMENT)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
 			CommentJDBC.unrateComment(uid, cid);
-		}else if(f.equals(DigestParameters.ADD_TOPIC_TO_CHANNEL)){
+		} else if (f.equals(DigestParameters.ADD_TOPIC_TO_CHANNEL)) {
 			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
 			int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
 			ChannelJDBC.addTopicToChannel(tid, cid);
-		}else if(f.equals(DigestParameters.GET_CHANNEL)){
-			if(req.getParameter(DigestParameters.CID) !=null ){
+		} else if (f.equals(DigestParameters.GET_CHANNEL)) {
+			if (req.getParameter(DigestParameters.CID) != null) {
 				int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
 				resp.getWriter().append(ChannelJDBC.getChannel(cid));
-			}else{
+			} else {
 				int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
-				resp.getWriter().append(""+ChannelJDBC.getChannelTid(tid));
+				resp.getWriter().append("" + ChannelJDBC.getChannelTid(tid));
 			}
-			
-		}else if(f.equals(DigestParameters.GET_TOPICS_FROM_CHANNEL)){
-				int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
-				resp.getWriter().append(ChannelJDBC.getTopicsOfChannel(cid));			
-		}else if(f.equals(DigestParameters.GET_CHANNELS_OF_USER)){
+
+		} else if (f.equals(DigestParameters.GET_TOPICS_FROM_CHANNEL)) {
+			int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
+			resp.getWriter().append(ChannelJDBC.getTopicsOfChannel(cid));
+		} else if (f.equals(DigestParameters.GET_CHANNELS_OF_USER)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			resp.getWriter().append(ChannelJDBC.getChannelsOfUser(uid));
-		}else if(f.equals(DigestParameters.MARK_COMMENT_AS_QUESTION)){
+		} else if (f.equals(DigestParameters.MARK_COMMENT_AS_QUESTION)) {
 			int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
 			CommentJDBC.updateType(cid, CommentJDBC.QUESTION);
-		}else if(f.equals(DigestParameters.MARK_COMMENT_AS_INSTRUCTIVE)){
+		} else if (f.equals(DigestParameters.MARK_COMMENT_AS_INSTRUCTIVE)) {
 			int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
 			CommentJDBC.updateType(cid, CommentJDBC.INSTRUCTIVE);
-		}else if(f.equals(DigestParameters.ADD_PROGRESS)){
+		} else if (f.equals(DigestParameters.ADD_PROGRESS)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
 			ProgressJDBC.addProgres(uid, tid);
-		}else if(f.equals(DigestParameters.GET_PROGRESS_TOPIC)){
+		} else if (f.equals(DigestParameters.GET_PROGRESS_TOPIC)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
-			resp.getWriter().append(""+ProgressJDBC.getProgressOnTopic(tid, uid));
-		}else if(f.equals(DigestParameters.SET_PROGRESS)){
+			resp.getWriter().append("" + ProgressJDBC.getProgressOnTopic(tid, uid));
+		} else if (f.equals(DigestParameters.SET_PROGRESS)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			int tid = Integer.parseInt(req.getParameter(DigestParameters.TID));
 			int val = Integer.parseInt(req.getParameter(DigestParameters.VAL));
 			ProgressJDBC.setProgressTopic(tid, uid, val);
-		}else if(f.equals(DigestParameters.GET_PROGRESS_CHANNEL)){
+		} else if (f.equals(DigestParameters.GET_PROGRESS_CHANNEL)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			int cid = Integer.parseInt(req.getParameter(DigestParameters.CID));
-			resp.getWriter().append(""+ProgressJDBC.getProgressOnChannel(cid, uid));
-		}else if(f.equals(DigestParameters.GET_SUBSCRIBED_CHANNELS)){
+			resp.getWriter().append("" + ProgressJDBC.getProgressOnChannel(cid, uid));
+		} else if (f.equals(DigestParameters.GET_SUBSCRIBED_CHANNELS)) {
 			int uid = Integer.parseInt(req.getParameter(DigestParameters.UID));
 			resp.getWriter().append(ChannelJDBC.getSubscribedChannels(uid));
-		}else if(f.equals(DigestParameters.GET_TRENDING_TOPICS)){
+		} else if (f.equals(DigestParameters.GET_TRENDING_TOPICS)) {
 			resp.getWriter().append(TopicJDBC.getTrendingTopics());
-		}else if(f.equals(DigestParameters.SEARCH_TOPICS)){
+		} else if (f.equals(DigestParameters.SEARCH_TOPICS)) {
 			String text = req.getParameter(DigestParameters.TEXT);
 			resp.getWriter().append(TopicJDBC.getTopicWithString(text));
-		} else if (f.equals("test")){
+		} else if (f.equals("test")) {
 			ArrayList<TopicTag> tag = new ArrayList<TopicTag>();
 			tag.add(new TopicTag("test(exam)"));
 			tag.add(new TopicTag("apple(fruit)"));
 			Topic topic = new Topic(0, "header", "image", "body", 1, 0, tag, null, null, null, null);
-			 
+
 			System.out.println(TopicJDBC.createTopic(topic));
 		}
-		
+
 		else {
 			resp.getWriter().append("Welcome to Digest API");
 		}
-			
-		
+
 		// doPost(req, resp);
 	}
-
+	/**
+	 * Handles post requests.
+	 * @param req Request.
+	 * @param resp Response.
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -297,13 +300,13 @@ public class DigestAPIServlet extends HttpServlet {
 			Topic topic = gson.fromJson(bufferedReader, Topic.class);
 			int tid = TopicJDBC.createTopic(topic);
 			if (tid != -1) {
-				//resp.setStatus(200);
+				// resp.setStatus(200);
 				resp.getWriter().append("" + tid);
 			} else {
 				resp.setStatus(400);
 			}
 
-		}else{
+		} else {
 			doGet(req, resp);
 		}
 		/*
@@ -313,7 +316,7 @@ public class DigestAPIServlet extends HttpServlet {
 		 * //resp.getWriter().append(role.getName());
 		 * resp.getWriter().append(role.getName()); }
 		 */
-		// 
+		//
 		/*
 		 * try {
 		 * 
@@ -327,7 +330,12 @@ public class DigestAPIServlet extends HttpServlet {
 		 * e.printStackTrace(); }
 		 */
 	}
-
+	/**
+	 * @deprecated
+	 * Makes hexadecimal number from byte array
+	 * @param hash Byte array.
+	 * @return Hexadecimal number as <String>
+	 */
 	private static String byteToHex(final byte[] hash) {
 		Formatter formatter = new Formatter();
 		for (byte b : hash) {
@@ -338,9 +346,6 @@ public class DigestAPIServlet extends HttpServlet {
 		return result;
 	}
 
-	private static String invalidSession() {
-		return "Invalid session";
-
-	}
+	
 
 }
--- digest.api/src/org/bounswe/digest/api/DigestParameters.java
+++ digest.api/src/org/bounswe/digest/api/DigestParameters.java
@@ -1,14 +1,38 @@
 package org.bounswe.digest.api;
 
 public class DigestParameters {
+	/**
+	 * 
+	 */
 	public static final String FUNC = "f";
+	/**
+	 * 
+	 */
 	public static final String USERNAME = "username";
+	/**
+	 * 
+	 */
 	public static final String PASSWORD = "password";
+	/**
+	 * 
+	 */
 	public static final String EMAIL = "email";
+	/**
+	 * 
+	 */
 	public static final String FIRST_NAME = "first_name";
+	/**
+	 * 
+	 */
 	public static final String LAST_NAME = "last_name";
+	/**
+	 * 
+	 */
 	public static final String STATUS = "status";
-
+	/**
+	 * 
+	 */
+	
 	public static final String LOGIN = "login";
 	public static final String REGISTER = "register";
 	public static final String CREATE_TOPIC = "create_topic";
